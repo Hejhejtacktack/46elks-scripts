427e2df38975a61ef97a24e3fcc55b09b7596e35
Hejhejtacktack <114874962+Hejhejtacktack@users.noreply.github.com>
2024-11-20 09:19:11 +0100

    reformat code

--- send-sms.py
+++ send-sms.py
@@ -1,30 +1,33 @@
 import requests
 import argparse
 
-parser = argparse.ArgumentParser(description="Send an SMS containing using 46elks API.")
+parser = argparse.ArgumentParser(description="Send an SMS using 46elks API.")
 parser.add_argument('api_username', type=str, help="Your 46elks API username (provided by 46elks).")
 parser.add_argument('api_password', type=str, help="Your 46elks API password (provided by 46elks).")
-parser.add_argument('receiver_phone_number', type=str, help="Receiver phone number in E.164 format, e.g., +46701234567.")
+parser.add_argument('receiver_phone_number', type=str,
+                    help="Receiver phone number in E.164 format, e.g., +46701234567.")
 
 # Optional argument for custom message
-parser.add_argument('--message', type=str, default="Hej", help="The message to send. Enclose the message in quotes if it contains spaces. Default is 'Hej'.")
-parser.add_argument('--sender', type=str, default="Elks", help="The sender's name. Enclose the message in quotes if it contains spaces. Default is 'Elks'.")
+parser.add_argument('-m', '--message', type=str, default="Hej",
+                    help="The message to send. Enclose the message in quotes if it contains spaces. Default is 'Hej'.")
+parser.add_argument('--sender', type=str, default="Elks",
+                    help="The sender's name. Enclose the message in quotes if it contains spaces. Default is 'Elks'.")
 
 args = parser.parse_args()
 
-API_USERNAME = args.api_username
-API_PASSWORD = args.api_password
+api_username = args.api_username
+api_password = args.api_password
 receiver = args.receiver_phone_number
 message = args.message
 sender = args.sender
 
 response = requests.post(
-  'https://api.46elks.com/a1/sms',
-  auth = (API_USERNAME, API_PASSWORD),
-  data = {
-    'from': sender,
-    'to': receiver,
-    'message': message
-  }
+    'https://api.46elks.com/a1/sms',
+    auth=(api_username, api_password),
+    data={
+        'from': sender,
+        'to': receiver,
+        'message': message
+    }
 )
 print(response.text)
