1cb0512a1563fee9275c5ef003c6e0c77e0b3f2b
Hejhejtacktack <114874962+Hejhejtacktack@users.noreply.github.com>
2024-11-20 16:18:58 +0100

    able to pass a file path to a csv file as argument update receivers help removed csv-reader.py

--- send-sms.py
+++ send-sms.py
@@ -1,11 +1,13 @@
 import requests
 import argparse
+from utils.converter import csv_to_list
+
 
 parser = argparse.ArgumentParser(description="Send an SMS using 46elks API.")
 parser.add_argument('api_username', type=str, help="Your 46elks API username (provided by 46elks).")
 parser.add_argument('api_password', type=str, help="Your 46elks API password (provided by 46elks).")
-parser.add_argument('receiver_phone_number', type=str,
-                    help="Receiver phone number in E.164 format, e.g., +46701234567.")
+parser.add_argument('receivers', type=str,
+                    help="Receiver phone number in E.164 format (e.g., +46701234567) or a path to a CSV file with phone numbers.")
 
 # Optional argument for custom message
 parser.add_argument('-m', '--message', type=str, default="Hej",
@@ -17,17 +19,21 @@ args = parser.parse_args()
 
 api_username = args.api_username
 api_password = args.api_password
-receiver = args.receiver_phone_number
+receivers = args.receivers  # Str containing a phone number or a file path
 message = args.message
 sender = args.sender
 
-response = requests.post(
-    'https://api.46elks.com/a1/sms',
-    auth=(api_username, api_password),
-    data={
-        'from': sender,
-        'to': receiver,
-        'message': message
-    }
-)
-print(response.text)
+# Makes receivers a list
+receivers = [receivers] if receivers.startswith("+") else csv_to_list(receivers)
+
+for phone_number in receivers:
+    response = requests.post(
+        'https://api.46elks.com/a1/sms',
+        auth=(api_username, api_password),
+        data={
+            'from': sender,
+            'to': phone_number,
+            'message': message
+        }
+    )
+    print(response.text)
