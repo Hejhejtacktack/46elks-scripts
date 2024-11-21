6d2da518d90e2d4d725bcc63a1300cf72315bd9d
Hejhejtacktack <114874962+Hejhejtacktack@users.noreply.github.com>
2024-11-21 12:08:33 +0100

    Add documentation

--- utils/converter.py
+++ utils/converter.py
@@ -2,6 +2,14 @@ import csv
 
 
 def csv_to_list(file_path):
+    """
+    Takes a file path to a csv file and returns a list of lists.
+    :param file_path: the file path of the csv file
+    :return: a list of strings hopefully containing phone numbers
+    :rtype: list(string)
+    :raise FileNotFoundError: if the file does not exist
+    :raise OSError: if the file cannot be read
+    """
     try:
         with open(file_path, mode='r') as file:
             rows = [row[0] for row in csv.reader(file)]
