a3d2d429c77625d9b2fa80b22ab68c2bd61b34a4
Hejhejtacktack <114874962+Hejhejtacktack@users.noreply.github.com>
2024-11-26 15:06:44 +0100

    able to automaticlly reply or forward using CLI
    user is able to choose if recieved sms will be replied or forwarded using arsparse
    
    https://46elks.se/docs/receive-sms

--- receive-sms.py
+++ receive-sms.py
@@ -1,12 +1,28 @@
 from bottle import request, post, run
 import json
+import argparse
 
+
+parser = argparse.ArgumentParser(description='Receive SMS messages through 46elks API. '
+                                             'This script is supposed to run a simple web server so your 46elks phone number can receive sms. '
+                                             'This may be a weird script, I know')
+
+parser.add_argument("actions",
+                    nargs=2,
+                    type=str,
+                    default='send',
+                    help="Action the script takes when received a sms, "
+                         "'reply' or 'forward' "
+                         "followed by either the reply message or forward phone number in E.164 format (e.g., +46701234567)")
+
+args = parser.parse_args()
 
 @post('/sms')
 def sms():
     print("You have received an SMS")
     return {
-        "reply": "Thank you for your message."}
+        args.actions[0] : args.actions[1]
+    }
 
 
 run(host='0.0.0.0', port=8080)
