5ddd23f249a2b8c34f89a0a7f1914cda16334b39
Hejhejtacktack <114874962+Hejhejtacktack@users.noreply.github.com>
2024-11-26 11:09:50 +0100

    refactor code

--- receive-sms.py
+++ receive-sms.py
@@ -1,10 +1,12 @@
 from bottle import request, post, run
 import json
 
+
 @post('/sms')
 def sms():
-  print("You have received an SMS")
-  return {
-    "reply": "Thank you for your message."}
+    print("You have received an SMS")
+    return {
+        "reply": "Thank you for your message."}
+
 
 run(host='0.0.0.0', port=8080)
