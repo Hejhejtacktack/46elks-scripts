a269d33d509beaaf7bfa4d589757996b19998c67
Hejhejtacktack <114874962+Hejhejtacktack@users.noreply.github.com>
2024-12-03 10:07:21 +0100

    able to use IVR
    still does not work as specification

--- ivr.py
+++ ivr.py
@@ -3,22 +3,23 @@ import os
 import requests
 from dotenv import load_dotenv
 from flask import Flask, request
-from make_and_forward_call import call
 
 app = Flask(__name__)
 
 load_dotenv()
 
+auth = (os.getenv("API_USERNAME"), os.getenv("API_PASSWORD"))
+
 @app.route('/ivr/incoming-calls', methods=['POST'])
 def incoming_calls():
     print(request.form)
     response = {
-        'ivr' : 'https://46elks.com/static/sound/voiceinfo.mp3', # TODO
-        'digits' : '1',
-        'next' : '/ivr/choice' # TODO
+        'ivr': 'https://46elks.com/static/sound/voiceinfo.mp3',
+        'digits': '1',
+        'next': 'https://63e5-94-234-96-85.ngrok-free.app/ivr/choice'
     }
-
     return json.dumps(response)
+
 
 @app.route('/ivr/choice', methods=['POST'])
 def ivr():
@@ -26,12 +27,17 @@ def ivr():
     if request.form['result'] == '1':
         # SPELA LJUDFIL OCH SKICKA SMS
         response = {
-            'play' : 'https://www.46elks.com/static/sound/smsinfo.mp3'
+            'play': 'https://www.46elks.com/static/sound/smsinfo.mp3'
         }
         return json.dumps(response)
     if request.form['result'] == '2':
-        # RING JOAKIM
-        call(os.getenv("API_USERNAME"), os.getenv("API_PASSWORD"))
+        # Calls the specified key in .env
+        response = {
+            'connect': os.getenv('PHONE_NUM_CAROLINE'),
+            'callerid': request.form['from']
+        }
+        return json.dumps(response)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
